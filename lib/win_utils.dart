e1e38c89aa8ff02574be72626656cf8795fb0b49
GOLDCODERS <ceo@goldcoders.dev>
2021-12-03 20:12:08 +0800

    export files we need to access using this package

--- lib/win_utils.dart
+++ lib/win_utils.dart
@@ -1,13 +1,5 @@
+library win_utils;
 
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class WinUtils {
-  static const MethodChannel _channel = MethodChannel('win_utils');
-
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
+// export 'src/run_as.dart';
+export 'src/models/path_env.dart';
+// export 'src/refresh_env.dart';
